e864ee8c44990dd8cbb9e9b7e45575bb9aca677b
梁丹 <751179350@qq.com>
2025-03-14 12:40:23 +0800

    add

--- src/app/(hero)/page.tsx
+++ src/app/(hero)/page.tsx
@@ -1,7 +1,5 @@
-import Hero from "@/components/hero";
-import homeSrc from "../../../public/images/home.jpeg";
 export default function Home() {
   return (
-    <Hero content="Model Training Platform" imgSrc={homeSrc} imgAlt="home" />
+    <div>Home</div>
   );
 }
--- src/app/(hero)/train/page.tsx
+++ src/app/(hero)/train/page.tsx
@@ -1,7 +1,6 @@
-import Hero from "@/components/hero";
-import trainSrc from "../../../../public/images/train.jpeg";
+
 export default function Home() {
   return (
-    <Hero content="train" imgSrc={trainSrc} imgAlt="train" />
+    <div>Train</div>
   );
 }
